31836b11b237bedf2444468c83f308157c6c2af8
Tiffany Chang <tifchang@wharton.upenn.edu>
2018-07-25 21:58:51 -0700

    app bp

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// App.js
+// Server route setup
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -16,3 +19,31 @@ var { CLIENT_EVENTS, RTM_EVENTS, RtmClient, WebClient } = require('@slack/client
 // Source the token variables 
 var rtm = new RtmClient(process.env.BOT_TOKEN);
 var web = new WebClient(process.env.BOT_TOKEN);
+
+// express server setup
+var app = express();
+var port = process.env.PORT || 3000;
+
+// TODO: require in google cal functions or anything cal related
+
+// Use body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// test route
+app.get('/hello', function (req, res) {
+    const code = req.query.code;
+    res.send('Hello world!');
+});
+
+// connection success route
+app.get('/connect/success', function (req, res) {
+    res.send('Connect success.')
+});
+
+// TODO: google authorization endpoint
+
+
+app.listen(port, function () {
+    console.log('Listening on port ' + port);
+    
+})
