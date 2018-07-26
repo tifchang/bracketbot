600f4903189de81e22fbdda729882dbac38735bb
Cesar Ibarra <cibarra@atlassian.com>
2018-07-26 16:28:12 -0700

    mend

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const SlackBot = require('slackbots');
 // Google OAuth instantiation
 // var OAuth2 = google.auth.OAuth2;
 
+const BracketBuilder = require('./bracketBuilder');
+
+const bb = new BracketBuilder();
+bb.addSingleParticipant({tournamentId: "4852317", name:"nora"}).then(res => console.log(res));
+
+
 // express server setup
 var app = express();
 var port = process.env.PORT || 3000;
