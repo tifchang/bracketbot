71e571a33c4c829d099ce4f56e4598a0578d2f25
Tiffany Chang <tifchang@wharton.upenn.edu>
2018-07-26 13:52:42 -0700

    working messages, saved user map

--- app.js
+++ app.js
@@ -8,20 +8,40 @@ var google = require('googleapis');
 var googleAuth = require('google-auth-library');
 const SlackBot = require('slackbots');
 
+// Google OAuth instantiation
+// var OAuth2 = google.auth.OAuth2;
+
+// express server setup
+var app = express();
+var port = process.env.PORT || 3000;
+
 // bot
 const bot = new SlackBot({
     token: process.env.BOT_TOKEN,
     name: 'LilBB'
 })
 
+// Object that stores mapping of uesrs
+var all_users = {};
+
 bot.on('start', () => {
     const params = {
         icon_emoji: ''
     }
-    bot.postMessageToChannel(
-        'general', 
-        ":ok_hand: :gentlyplz: :tt: ~ You are what you yeet ~ :tt: :gentlyplz: :ok_hand:", 
-        params);
+    // user storage
+    var users = bot.getUsers()._value.members;
+    users.forEach(u => {
+        all_users[u.id] = {
+            name: u.name || '',
+            real_name: u.real_name || '',
+            display_name: u.display_name || '',
+            email: u.profile.email || '',
+        }
+    });
+    // bot.postMessageToChannel(
+    //     'general', 
+    //     ":ok_hand: :gentlyplz: :tt: ~ You are what you yeet ~ :tt: :gentlyplz: :ok_hand:", 
+    //     params);
 })
 
 // Error handler
@@ -30,18 +50,33 @@ bot.on('error', (err) => console.log(err))
 // Message handler 
 bot.on('message', (data) => {
     if (data.type !== 'message') {
-        console.log("data type: " + data.type);
         return;
     }
-    console.log(data);
+    // Create tournament
+    var msgArray = data.text.split(" ");
+    if (msgArray[1] === "create") {
+        var bracketName = "";
+        for (i = 2; i < msgArray.indexOf("bracket"); i++) {
+            bracketName += (msgArray[i] + " ");
+        }
+        console.log(bracketName.trim());
+        // createTournament();
+    }
+    // Add players
+    if (msgArray[1] === "add") {
+
+    }
 })
-// Google OAuth instantiation
-// var OAuth2 = google.auth.OAuth2;
+
+// Message helper functions
+function createTournament() {
+    // TODO: insert function to create tournament
+    bot.postMessageToChannel(
+        'general', 
+        "Created tournament [TOURNAMENT NAME] [TOURNAMENT ID]");
+}
 
 
-// express server setup
-var app = express();
-var port = process.env.PORT || 3000;
 
 // TODO: require in google cal functions or anything cal related
 
@@ -60,10 +95,6 @@ app.get('/connect/success', function (req, res) {
 });
 
 // TODO: google authorization endpoint
-
-// TODO: challonge api create bracket
-
-// TODO: challenge api update bracket
 
 
 app.listen(port, function () {
