86316ef4c8da46d1c304af0e9dcaf17f31c41186
Nora McGinley <nmcginley@atlassian.com>
2018-07-26 19:28:00 -0700

    INCOMPLETE google cal logic

--- calendarHelper.js
+++ calendarHelper.js
@@ -6,7 +6,6 @@ var API_KEY = 'AIzaSyAPMqDWheRXuFixwbVWEVcXH2tb27UnzWM';
 var CLIENT_ID = '1051057973316-ej07u118rs2revga37m63vdp99fe3bnp.apps.googleusercontent.com'; //probably want to change at some point to draw from json
 var SCOPE = 'https://www.googleapis.com/auth/calendar';
 
-//originally executed when the page is loaded
 function init() {
 // Load the API's client and auth2 modules.
 // Call the initClient function after the modules load.
@@ -15,7 +14,6 @@ function init() {
 
 function initClient() {
     // Retrieve the discovery document for version 3 of Google Drive API.
-    // In practice, your app can retrieve one or more discovery documents.
     var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
 
     // Initialize the gapi.client object, which app uses to make API requests.
@@ -31,19 +29,119 @@ function initClient() {
     });
 }
 
-//Starts google auth flow??
+//Starts google auth flow
 function startAuthenticate(){
     GoogleAuth.signIn(); 
 }
 
-//originally called if user clicks logout
 function revokeAccess() {
     GoogleAuth.disconnect();
 }
 
 //after authenticate successfully, return the token and id to surya
 function getAuthSuccess() {
-
+    return
 }
 
-//actually look for gaps and add to calendar 
+function getGoogleAuth() {
+    var credentials = JSON.parse(process.env.CLIENT_SECRET);
+    var clientSecret = credentials.web.client_secret;
+    var clientId = credentials.web.client_id;
+
+    return new OAuth2(
+        clientId,
+        clientSecret
+    );
+}
+
+function appointmentHelper(user1, user2, startDate){
+    //TODO: 
+    //var calendar1 = user1. GET THE CALENDAR ID might have to use the list gcal api?? 
+    //var calendar2 = user2. GET THE CALENDAR ID
+
+    var freeBlocks = lookForTimes(calendar1, calendar2, startDate);
+    var targetDate = findAppointment(calendar1, calendar2, freeBlocks).toString(); 
+    return makeAppointment(user1, user2, calendar1, calendar2, targetDate); 
+}
+
+//makes a freebusy query to google cal api
+//params: the two participants' calendars and the datetime to start looking at
+//on success: returns an array of block objects, which hold a start datetime and end datetime as dates
+//on failure: sets the start datetime to 9am the next day and recursively calls itself
+function lookForTimes(calendar1, calendar2, startDate) {
+    //TODO: somewhere we need to deal with if the tournament is requested after 5:30pm
+    var endDate = new Date(startDate.getDate());
+    endDate.setTime
+
+    var resource = {
+        'timeMin': startDate.toISOString,
+        'timeMax': endDate.toISOString,
+        'groupExpansionMax': 2,
+        'calendarExpansionMax': 2,
+        'items': [
+        {
+            'id': calendar1,
+            'id': calendar2
+        }
+        ]
+    };
+    calendar.freebusy.query({                   //does this do anything? fuck if I know
+        resource: resource
+    })
+    //TODO: error handling?
+
+    //TODO:
+    // if the freebusy array got populated
+    //     return pendingMeeting = array of start and end times as objects which contain the start and end times as dates 
+    // else   
+    //     var newStart = 9am the day after the start date
+    //     return lookForTimes(calendar1, calendar2, newStart);
+}
+
+//looks for a suitable appointment block within the free blocks 
+//params: the two participants' calendars and the list of freeBlock objects which contain the start and end times as dates
+//on success: returns the start datetime of the block that is free and at least 15 minutes long
+//on failure: that means there is no free block that's long enough, so it calls lookForTimes with tomorrow as the next start date 
+function findAppointment(calendar1, calendar2, freeBlocks){
+    freeBlocks.foreach((block) => {
+        var start = new Date(block.startTime);
+        var end = new Date(block.endTime); 
+        var timeDiff = Math.abs(end.getTime() - start.getTime());
+        var diffMins = Math.ceil(timeDiff / (60000));
+        if(diffMins >= 15){
+            return start;
+        }
+    });
+    var newStart = new Date(); 
+    return lookForTimes(calendar1, calendar2, newStart); 
+}
+
+//actually makes an appointment in gcal
+//returns a success message and the datetime of the new appointment or a failure message
+function makeAppointment(user1, user2, calendar1, calendar2, targetDate){
+    var googleAuthorization = getGoogleAuth(); 
+    //TODO: 
+    var userArr = []; 
+    //userArr.push({displayName: , email: }); ADD THE USERS WITH THEIR NAMES AND EMAILS TO THE ARRAY
+    //userArr.push({displayName: , email: })
+    var endDate = new Date(targetDate); 
+    //add fifteen minutes to the endDate
+
+    var event = {
+        'start': {
+          'dateTime': targetDate.toISOString()
+        },
+        'end': {
+          'dateTime': endDate.toISOString()
+        },
+        'attendees': userArr
+    };
+    calendar.events.insert({
+    auth: googleAuthorization,
+    calendarId: 'primary',                  //primary keyword used for accessing primary calendar of the currently logged user, otherwise use gcal api list 
+    resource: event,
+    })
+    //TODO: error handling? 
+
+    return "The appointent was set for "+targetDate+"!"; 
+}
--- env.sh
+++ env.sh
@@ -1,2 +1,7 @@
 export BOT_TOKEN='xoxb-405402171509-405402907621-YDDNr4M7mVrmfMsWaxlHyLMS';
 export CHALLONGE_KEY='SfEhJrMKG2dmj27FXj1Qj7ZOhw4b0lNkuslSEr1J';
+<<<<<<< Updated upstream
+=======
+export CLIENT_SECRET='{"web":{"client_id":"1051057973316-ej07u118rs2revga37m63vdp99fe3bnp.apps.googleusercontent.com","project_id":"intern-shipit-bracketify","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"sX0qbxK_SshqF5YS4vcT8zYi","javascript_origins":["http://ee782d14.ngrok.io"]}}';
+
+>>>>>>> Stashed changes
